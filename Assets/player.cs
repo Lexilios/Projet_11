06969da8211a31b89ac0831903e321f7687b0751
Maxime.C <macorona.41@gmail.com>
2024-02-29 13:00:42 +0100

    Avancement
    Réglage mouvement grandeur map et grandeur balle

--- Assets/player.cs
+++ Assets/player.cs
@@ -6,19 +6,37 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    [SerializeField] private string horizontalInput;
+    [SerializeField] private string verticalInput;
+    Rigidbody rb;
+
+    [SerializeField] float movementSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(transform.up, Input.GetAxis("VerticalPlayer1") * 0.5f);
-        transform.position = transform.position + transform.forward * 5 * Input.GetAxis("HorizontalPlayer1") * Time.deltaTime;
-
+        ControllPlayer();
     }
+        /* transform.Rotate(transform.up, Input.GetAxis("VerticalPlayer1") * 0.5f);
+         transform.position = transform.position + transform.forward * 5 * Input.GetAxis("HorizontalPlayer1") * Time.deltaTime;*/
+        void ControllPlayer()
+        {
+            float moveHorizontal = Input.GetAxis(horizontalInput);
+            
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f);
+            transform.Rotate(transform.up, Input.GetAxis(verticalInput) * 0.5f);
+
+
+            transform.Translate(transform.forward * moveHorizontal * movementSpeed * Time.deltaTime, Space.World);
+        }
+
+    
 
     private void FixedUpdate()
     {
@@ -26,3 +44,7 @@ public class player : MonoBehaviour
     }
 
 }
+
+
+
+
